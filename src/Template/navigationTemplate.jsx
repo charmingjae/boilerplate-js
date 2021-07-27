fa20c201bfd78f511ed171a32e69b117db3702bf
chaminjae <beingeskimo@gmail.com>
2021-07-27 13:41:32 +0900

    Create storyboard menu

--- src/Template/navigationTemplate.jsx
+++ src/Template/navigationTemplate.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import DefaultButton from "@comp/Button/defaultButton";
+import { divWrapper, item } from "@css/navigationTemplate.module.css";
 
 function NavigationTemplate() {
-  return <div>Navigation</div>;
+  return (
+    <div className={`${divWrapper}`}>
+      <div className={`${item}`}>
+        <a href="/">Boilerplate</a>
+      </div>
+      <div className={`${item}`}>
+        <DefaultButton btnText={"_Component"} />
+      </div>
+    </div>
+  );
 }
 export default NavigationTemplate;
